22cc0fc16665dcc023eeb904783ced4e4752fbfd
forel <mangolias@mangolias.com>
2025-05-31 11:51:20 +0300

    count attempt

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,49 +1,44 @@
-use std::cmp::Ordering; 
+use std::cmp::Ordering;
 use std::io;
-
 use rand::Rng;
+use rand::rng;
 
 fn main() {
-    // Launch the game
-    guessing_game();
-}
-
-fn guessing_game() {
-    println!("Hello and welcome to the guessing game!");
-
-    // Generate a random number between 1 and 100
-    let secret_number = rand::rng().random_range(1..=100);
-
-    // Launch the loop
+    println!("🎯 Welcome to the guessing game!\n");
+    let secret_number = rng().random_range(1..=100); // Generate a random number between 1 and 100
+    println!("🤔 I have selected a secret number between 1 to 100. Can you guess it?");
+    let mut attempts = 0; // Initialize attempts to 0
     loop {
-        println!("Please input your guess (between 1 and 100):");
+        if attempts == 0{
+            println!("Enter your guess (1–100):");
+        }else{
+            println!("Attempt {}: Enter your guess (1–100):", attempts + 1);
+        }
 
-        // Create a mutable string to store input
-        let mut guess = String::new();
-
-        // Read the input value
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        // Parse the input to u32
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Please type a number!");
+        let guess = match read_user_input() {
+            Some(num) => num,
+            None => {
+                println!("\n⚠️ Invalid input. Please enter a valid number.");
                 continue;
             }
         };
+    attempts += 1;
 
-        println!("You guessed: {}", guess);
+        println!("🔢 You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => println!("\nToo small!"),
+            Ordering::Greater => println!("\nToo big!"),
             Ordering::Equal => {
-                println!("Congratulations! You guessed the number: {}", secret_number);
+            println!("🎉 Correct! The number was: {}\n\n🧮 You guessed it in {} attempts!", secret_number, attempts);
                 break;
             }
         }
     }
+}
+
+fn read_user_input() -> Option<u32> {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).ok()?;
+    input.trim().parse::<u32>().ok()
 }
